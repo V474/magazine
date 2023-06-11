d6fc456aa3c1ad1b52a7f6a4c117490f998b6fe5
V474 <9835799@gmail.com>
2023-06-11 14:06:48 +0300

    class in js

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const magic = () => new Date();
-console.log(magic());
+// setter and getter
+class Thermostat {
+    constructor(f) {
+      let c = 5/9 * (f - 32);
+      this._f = c;
+    }
+    get temperature() {
+      return this._f;
+    }
+    set temperature(c) {
+      return this._f = c;
+    }
+  }
+  
+  const thermos = new Thermostat(76); // Налаштування у шкалі Фаренгейта
+  let temp = thermos.temperature; // 24.44 градусів за Цельсієм
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 градусів за Цельсієм
